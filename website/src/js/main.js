a7de094c8b9048c1ba7468240adc400c2e4f6c4c
Zamt3x <oyvind.finsnes@hotmail.com>
2020-11-08 05:25:56 +0100

    Updated main.js to use object orientation for improved structure.

--- website/src/js/main.js
+++ website/src/js/main.js
@@ -1,16 +1,40 @@
-function createLogEntry(name, text) {
+function Interface() {
+    this.txtUserInput = document.getElementById("userInput");
+    this.btnUserSubmit = document.getElementById("userSubmit");
+    this.divChatLog = document.getElementById("chatLog");
+    this.botCanvas = document.getElementById("botCanvas");
+}
+
+Interface.prototype.init = function () {
+    var self = this;
+
+    this.txtUserInput.addEventListener("keyup", function (e) {
+        if (e.code === "Enter" && !e.shiftKey) {
+            self.handleUserInput();
+        }
+    });
+
+    this.btnUserSubmit.addEventListener("click", this.handleUserInput);
+
+    this.botCanvas.classList.add("visible");
+    this.txtUserInput.disabled = false;
+}
+
+Interface.prototype.createLogEntry = function (name, text) {
     var node = document.createElement("span");
-    var logElem = document.getElementById("chatLog");
 
     node.setAttribute("class", name);
+    // Capitalizing the first letter of the name
     node.setAttribute("data-name", name.charAt(0).toUpperCase() + name.slice(1));
     node.textContent = text;
 
-    logElem.appendChild(node);
-    logElem.scrollTop = logElem.scrollHeight;
+    this.divChatLog.appendChild(node);
+    // Scroll the chat log to bottom on new entry
+    this.divChatLog.scrollTop = this.divChatLog.scrollHeight;
 }
 
-function fetchBotResponse(input) {
+Interface.prototype.fetchBotResponse = function (input) {
+    var self = this;
     var xhttp = new XMLHttpRequest();
     var url = "src/pages/response-api.php";
     var params = "input=" + String(input);
@@ -18,40 +42,36 @@ function fetchBotResponse(input) {
     xhttp.open("POST", url, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     
+    // As a variable was created to preserve "class this", we can use
+    // both self and this to refer to the correct objects
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            createLogEntry("bot", this.responseText);
+            self.createLogEntry("bot", this.responseText);
         }
     };
 
     xhttp.send(params);
 }
 
-function handleUserInput() {
-    var elem = document.getElementById("userInput");
+Interface.prototype.handleUserInput = function () {
+    // Lock the text input until a response is given
+    this.txtUserInput.disabled = true;
 
-    if (elem.value.trim() !== "") {
-        createLogEntry("user", elem.value);
-        fetchBotResponse(elem.value);
+    if (this.txtUserInput.value.trim() !== "") {
+        this.createLogEntry("user", this.txtUserInput.value);
+        this.fetchBotResponse(this.txtUserInput.value);
     }
 
-    elem.value = "";
-    elem.focus();
+    this.txtUserInput.disabled = false;
+    this.txtUserInput.value = "";
+    this.txtUserInput.focus();
 }
 
-function addChatInputListeners() {
-    document.getElementById("userInput").addEventListener("keyup", function (e) {
-        if (e.code === "Enter" && !e.shiftKey) {
-            handleUserInput();
-        }
-    });
-
-    document.getElementById("userSubmit").addEventListener("click", handleUserInput);
-}
+var Interface = new Interface();
 
 function main() {
-    addChatInputListeners();
-    document.getElementById("botCanvas").classList.add("visible");
+    Interface.init();
+
     AnimationManager.enqueue("thoughtful-head-shake");
 }
 
