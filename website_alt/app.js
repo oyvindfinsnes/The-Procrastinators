80dffa49752233dc89921d425fab87681de0e82c
Zamt3x <oyvind.finsnes@hotmail.com>
2021-03-10 00:51:11 +0100

    Updated ParticleJS to use CDN instead of locally included file.

--- website_alt/app.js
+++ website_alt/app.js
@@ -1,19 +1,4 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-  
-  {
+particlesJS("particles-js", {
     "particles": {
       "number": {
         "value": 80,
@@ -128,6 +113,4 @@ particlesJS('particles-js',
       "background_repeat": "no-repeat",
       "background_size": "cover"
     }
-  }
-
-);
+});
