5a7e14177a8de9d1fa51af9fbd17ee35d3d3d45c
Zamt3x <oyvind.finsnes@hotmail.com>
2021-03-10 20:53:15 +0100

    Improved bot compiler to use the absolute path of Python executable.

--- response-api/compiler.py
+++ response-api/compiler.py
@@ -12,6 +12,7 @@ supported_env = {
     "linux": "Linux"
 }
 current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+python_path = sys.executable if sys.executable else "python"
 
 if env not in supported_env.values():
     raise NotImplementedError("Your OS is not supported, sorry!")
@@ -22,7 +23,7 @@ if os.path.isdir(os.path.join(current_dir, "dist")):
 
 if not os.path.isfile(os.path.join(current_dir, "src", "brain.brn")):
     try:
-        subprocess.run(["python", os.path.join(current_dir, "src", "builder.py")], cwd=current_dir)
+        subprocess.run([python_path, os.path.join(current_dir, "src", "builder.py")], cwd=current_dir)
     except Exception as e:
         print(f"The nested \"bot.py\" has failed to initialize: {str(e)}.")
         sys.exit(1)
@@ -31,7 +32,7 @@ if not os.path.isfile(os.path.join(current_dir, "src", "brain.brn")):
 # sources into a single executable.
 if env == supported_env["windows"]:
     try:
-        subprocess.run(["python", "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--noconsole", "--name", supported_env["windows"], "--add-data", "src/brain.brn;."], cwd=current_dir)
+        subprocess.run([python_path, "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--noconsole", "--name", supported_env["windows"], "--add-data", "src/brain.brn;."], cwd=current_dir)
         os.remove(os.path.join(current_dir, supported_env["windows"] + ".spec"))
     except Exception as e:
         print(f"The program has failed: {str(e)}. Check if required Python dependency \"pyinstaller\" or \"aiml\" is installed.")
@@ -41,7 +42,7 @@ if env == supported_env["windows"]:
 # 3.x for the project, we want to compile into a single executable.
 elif env == supported_env["macos"]:
     try:
-        subprocess.run(["python", "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--noconsole", "--name", supported_env["macos"], "--add-data", "src/brain.brn:."], cwd=current_dir)
+        subprocess.run([python_path, "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--noconsole", "--name", supported_env["macos"], "--add-data", "src/brain.brn:."], cwd=current_dir)
         os.remove(os.path.join(current_dir, supported_env["macos"] + ".spec"))
     except Exception as e:
         print(f"The program has failed: {str(e)}. Check if required Python dependency \"pyinstaller\" or \"aiml\" is installed.")
@@ -51,7 +52,7 @@ elif env == supported_env["macos"]:
 # distribution uses, we want to compile into a single executable for simplicity.
 elif env == supported_env["linux"]:
     try:
-        subprocess.run(["python", "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--name", supported_env["linux"], "--add-data", "src/brain.brn:."], cwd=current_dir)
+        subprocess.run([python_path, "-m", "PyInstaller", os.path.join(current_dir, "src", "bot.py"), "--name", supported_env["linux"], "--add-data", "src/brain.brn:."], cwd=current_dir)
         os.remove(os.path.join(current_dir, supported_env["linux"] + ".spec"))
     except Exception as e:
         print(f"The program has failed: {str(e)}. Check if required Python dependency \"pyinstaller\" or \"aiml\" is installed.")
--- website/src/pages/response-api.php
+++ website/src/pages/response-api.php
@@ -11,8 +11,6 @@
         $path = "{$path_base}{$path_end[$current_env]}";
         $user_input = "\"{$_POST['input']}\"";
 
-        putenv('LANG=en_US.UTF-8');
-
         $command = escapeshellcmd("{$path} {$user_input}");
         $output = shell_exec($command);
         
